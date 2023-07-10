f1bfc0843f0a707bebbbcc5e210d99a839a48999
pavsom <pavsome@mail.ru>
2023-07-10 17:40:20 +0300

    Версия 1.1. Исправлены ошибки, добавлена генерация .csv файла с кооридантами излучателей по итогам моделирования.

--- main.py
+++ main.py
@@ -1,35 +1,47 @@
 # Блок импортируемых библиотек
 import numpy as np
+import pandas as pd
 
 # Блок вводимых параметров
-type = str(input('Введите тип антенной решётки (rectangular - прямоугольная, hexagonal - гексагональная): '))
+array_type = str(input('Введите тип антенной решётки (rectangular - прямоугольная, hexagonal - гексагональная): '))
 num_x = int(input('Введите количество излучателей в строке: '))
 num_y = int(input('Ваедите количество излучателей в столбце: '))
 step_x = float(input('Введите шаг антенной решётки по горизонтали, см: '))
 step_y = float(input('Введите шаг антенной решётки по вертикали, см: '))
 
-x_coords = np.array([[.0] * num_x] * num_y)
-y_coords = np.array([[.0] * num_x] * num_y)
+# Создание пустых массивов для хранения координат по оси Х и Y излучателей
+x_cords = np.array([[.0] * num_x] * num_y)
+y_cords = np.array([[.0] * num_x] * num_y)
 
-match type:
+# Генерация координат излучателей по введённым параметрам с учётом выбора типа антенной решётки
+match array_type:
 
+    # Генерация координат излучателей для антенной решётки гексагонального типа
     case 'hexagonal':
         for i in range(num_y):
             for j in range(num_x):
                 if i % 2 == 0:
-                    x_coords[i, j] = j * step_x
-                    y_coords[i, j] = i * step_y
+                    x_cords[i][j] = j * step_x
+                    y_cords[i][j] = i * step_y
                 else:
-                    x_coords[i, j] = j * step_x + step_x / 2
-                    y_coords[i, j] = i * step_y + step_y / 2
+                    x_cords[i][j] = j * step_x + step_x / 2
+                    y_cords[i][j] = i * step_y
 
+    # Генерация координат излучателей для антенной решётки прямоугольного типа
     case 'rectangular':
         for i in range(num_y):
             for j in range(num_x):
-                x_coords[i, j] = j * step_x
-                y_coords[i, j] = i * step_y
+                x_cords[i][j] = j * step_x
+                y_cords[i][j] = i * step_y
 
+    # Вывод сообщения об ошибке в случае неверного выбора типа решётки
     case _:
         print('Введён неверный тип решётки')
 
-print('hah')
+# Слияние массивов координат X и Y в один массив, преобразование его в DataFrame и запись в .csv файл
+xy_cords = np.stack((x_cords.flatten(), y_cords.flatten()), axis=-1)
+header = ['X', 'Y']
+index = [i for i in range(num_x * num_y)]
+xy_df = pd.DataFrame(xy_cords, index, header)
+
+xy_df.to_csv('antenna_array.csv', index=False)
