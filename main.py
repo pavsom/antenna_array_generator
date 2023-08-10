ae51cfe0edd002811691514d30f339c198dc4990
pavsom <pavsome@mail.ru>
2023-08-10 20:46:20 +0300

    Исправление багов, добавлена таблица для примера. Интерфейс ещё не разработан

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import pandas as pd
 array_type = str(input('Введите тип антенной решётки (rectangular - прямоугольная, hexagonal - гексагональная): '))
 num_x = int(input('Введите количество излучателей в строке: '))
 num_y = int(input('Ваедите количество излучателей в столбце: '))
-step_x = float(input('Введите шаг антенной решётки по горизонтали, см: '))
-step_y = float(input('Введите шаг антенной решётки по вертикали, см: '))
+step_x = float(input('Введите шаг антенной решётки по горизонтали, см: ')) * 10
+step_y = float(input('Введите шаг антенной решётки по вертикали, см: ')) * 10
 
 # Создание пустых массивов для хранения координат по оси Х и Y излучателей
 x_cords = np.array([[.0] * num_x] * num_y)
@@ -44,4 +44,5 @@ header = ['X', 'Y']
 index = [i for i in range(num_x * num_y)]
 xy_df = pd.DataFrame(xy_cords, index, header)
 
-xy_df.to_csv('antenna_array.csv', index=False)
+name = 'antenna_array.csv'
+xy_df.to_csv(name, index=False)
